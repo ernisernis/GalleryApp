ee54b96f5068ff54d05a52bd9359cf73184728db
ernisernis <ernestas.dainys@gmail.com>
2022-01-23 13:31:49 +0000

    Added Label

--- app/src/main/java/uk/edu/le/co2103/javaprojectyr3/MainActivity.java
+++ app/src/main/java/uk/edu/le/co2103/javaprojectyr3/MainActivity.java
@@ -1,32 +1,14 @@
 package uk.edu.le.co2103.javaprojectyr3;
 
-import android.os.Bundle;
-
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.view.View;
-
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
-import uk.edu.le.co2103.javaprojectyr3.databinding.ActivityMainBinding;
-
-import android.view.Menu;
-import android.view.MenuItem;
+import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
-    private AppBarConfiguration appBarConfiguration;
-    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
 }
