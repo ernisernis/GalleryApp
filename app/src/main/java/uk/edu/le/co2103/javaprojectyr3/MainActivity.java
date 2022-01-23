eb3ef305560eb9af645ef4e2be1e716919adcfdd
ernisernis <ernestas.dainys@gmail.com>
2022-01-23 14:06:48 +0000

    Navigation added through 2 screens

--- app/src/main/java/uk/edu/le/co2103/javaprojectyr3/MainActivity.java
+++ app/src/main/java/uk/edu/le/co2103/javaprojectyr3/MainActivity.java
@@ -2,13 +2,36 @@ package uk.edu.le.co2103.javaprojectyr3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText text;
+    Button clickMe;
+    String textHere;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        clickMe = findViewById(R.id.button);
+        text = findViewById(R.id.text);
+
+        clickMe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                textHere = text.getText().toString();
+                Intent intent = new Intent(MainActivity.this,SecondActivity.class);
+                intent.putExtra("Text", textHere);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/uk/edu/le/co2103/javaprojectyr3/SecondActivity.java
@@ -0,0 +1,35 @@
+package uk.edu.le.co2103.javaprojectyr3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+
+    TextView textreceived;
+    Button button2;
+    String text;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        text = getIntent().getExtras().getString("Text");
+        textreceived = findViewById(R.id.textReceived);
+        textreceived.setText(text);
+
+        button2 = findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SecondActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
