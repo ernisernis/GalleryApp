28714be4328787c18ce62e83181defb76257d7ca
ernisernis <ernestas.dainys@gmail.com>
2022-01-23 21:06:17 +0000

    finalized DB Helper class

--- app/src/main/java/uk/edu/le/co2103/javaprojectyr3/DBHelper/DBHelper.java
+++ app/src/main/java/uk/edu/le/co2103/javaprojectyr3/DBHelper/DBHelper.java
@@ -1,5 +1,7 @@
 package uk.edu.le.co2103.javaprojectyr3.DBHelper;
 
+import android.content.Context;
+
 import net.sqlcipher.database.SQLiteDatabase;
 import net.sqlcipher.database.SQLiteOpenHelper;
 
@@ -7,13 +9,35 @@ public class DBHelper extends SQLiteOpenHelper {
 
     private static DBHelper instance;
 
+    public static final int DATABASE_VER = 1;
+    private static final String DATABASE_NAME = "GalleryApp.db";
+
+    public static final String TABLE_NAME="CONTACTS";
+    public static final String COLUMN_EMAIL="EMAIL";
+
+    public static final String PASS_PHARSE = "!@#ABC"; //password encrypt
+
+    private static final String SQL_CREATE_TABLE_QUERY="CREATE TABLE "+TABLE_NAME+" ("+COLUMN_EMAIL+" TEXT PRIMARY KEY)";
+
+    private static final String SQL_DELETE_TABLE_QUERY="DROP TABLE IF EXISTS " + TABLE_NAME;
+
+    public DBHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VER);
+    }
+
+    static public synchronized DBHelper getInstance(Context context){
+        if (instance==null) instance = new DBHelper(context);
+        return instance;
+    }
+
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-
+        sqLiteDatabase.execSQL(SQL_CREATE_TABLE_QUERY);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-
+        sqLiteDatabase.execSQL(SQL_DELETE_TABLE_QUERY);
+        onCreate(sqLiteDatabase);
     }
 }
