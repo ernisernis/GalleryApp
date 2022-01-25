0611afa91b7a1a1ff0f13be23384e664625e1af2
ernisernis <ernestas.dainys@gmail.com>
2022-01-25 15:57:39 +0000

    Button to take a photo

--- app/src/main/java/uk/edu/le/co2103/javaprojectyr3/ThirdActivity.java
+++ app/src/main/java/uk/edu/le/co2103/javaprojectyr3/ThirdActivity.java
@@ -1,15 +1,19 @@
 package uk.edu.le.co2103.javaprojectyr3;
 
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ThirdActivity extends AppCompatActivity {
 
-    Button goBack;
+    Button goBack, tkPicture;
+    static final int REQUEST_IMAGE_CAPTURE = 1; //for camera
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +21,7 @@ public class ThirdActivity extends AppCompatActivity {
         setContentView(R.layout.activity_third);
 
         goBack = findViewById(R.id.button);
+        tkPicture = findViewById(R.id.btnTakePicture);
 
         goBack.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,6 +31,27 @@ public class ThirdActivity extends AppCompatActivity {
             }
         });
 
+        tkPicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dispatchTakePictureIntent();
+            }
+        });
 
+
+    }
+
+//    @Override
+//    protected void OnActivityResult(int requestCode, int resultCode, Intent data) {
+//
+//    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        try {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        } catch (ActivityNotFoundException e) {
+            Toast.makeText(ThirdActivity.this, "Error: " + e, Toast.LENGTH_LONG).show();
+        }
     }
 }
