c228e34141afe2304ab5808fd77feff050863d06
ernisernis <ernestas.dainys@gmail.com>
2022-01-26 17:20:15 +0000

    The button is functional which adds the taken image to the database

--- app/src/main/java/uk/edu/le/co2103/javaprojectyr3/ThirdActivity.java
+++ app/src/main/java/uk/edu/le/co2103/javaprojectyr3/ThirdActivity.java
@@ -1,30 +1,61 @@
 package uk.edu.le.co2103.javaprojectyr3;
 
-import android.content.ActivityNotFoundException;
+import android.Manifest;
 import android.content.Intent;
-import android.graphics.Bitmap;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
+
+import net.sqlcipher.database.SQLiteDatabase;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Blob;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.Scanner;
+
+import uk.edu.le.co2103.javaprojectyr3.DBHelper.DBHelper;
 
 public class ThirdActivity extends AppCompatActivity {
 
+    public static final int CAMERA_PERM_CODE = 121;
+    public static final int CAMERA_REQUEST_CODE = 131;
+    static final int REQUEST_TAKE_PHOTO = 1;
+    static final int REQUEST_IMAGE_CAPTURE = 1; //for camera
+
+    String currentPhotoPath;
     ImageView imageView;
     Button goBack, tkPicture;
-    static final int REQUEST_IMAGE_CAPTURE = 1; //for camera
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_third);
 
+        SQLiteDatabase.loadLibs(this);
         goBack = findViewById(R.id.button);
         tkPicture = findViewById(R.id.btnTakePicture);
+        imageView = findViewById(R.id.imageView);
 
         goBack.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -37,7 +68,8 @@ public class ThirdActivity extends AppCompatActivity {
         tkPicture.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                dispatchTakePictureIntent();
+                askCameraPermissions();
+//                dispatchTakePictureIntent();
             }
         });
 
@@ -45,23 +77,84 @@ public class ThirdActivity extends AppCompatActivity {
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            imageView = findViewById(R.id.imageView);
-            Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
-            imageView.setImageBitmap(imageBitmap);
+        if(requestCode == CAMERA_REQUEST_CODE) {
+                File f = new File(currentPhotoPath);
+                System.out.println(f);
+                System.out.println(Arrays.toString(readFile(currentPhotoPath)));
+                imageView.setImageURI(Uri.fromFile(f));
+                DBHelper.getInstance(ThirdActivity.this).insertNewImage(Arrays.toString(readFile(currentPhotoPath)));
 
         }
+    }
+    private void askCameraPermissions() {
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CAMERA}, CAMERA_PERM_CODE);
+        } else {
+//            openCamera();
+            dispatchTakePictureIntent();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == CAMERA_PERM_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // open the camera;
+                // openCamera();
+                dispatchTakePictureIntent();
+            } else {
+                Toast.makeText(this, "Need Camera Permission!", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+
+    private File createImageFile() throws IOException {
+        //creating timestamp of the file
+        String timestamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timestamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,
+                ".jpg",
+                storageDir
+        );
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
     }
 
     private void dispatchTakePictureIntent() {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // erorr
+            }
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this, "com.example.android.fileprovider", photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, CAMERA_REQUEST_CODE);
+            }
+    }
+    private byte[] readFile(String file) {
+        ByteArrayOutputStream bos = null;
         try {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-        } catch (ActivityNotFoundException e) {
-            Toast.makeText(ThirdActivity.this, "Error: " + e, Toast.LENGTH_LONG).show();
+            File f = new File(file);
+            FileInputStream fis = new FileInputStream(f);
+            byte[] buffer = new byte[1024];
+            bos = new ByteArrayOutputStream();
+            for (int len; (len = fis.read(buffer)) != -1;) {
+                bos.write(buffer, 0, len);
+            }
+        } catch (FileNotFoundException e) {
+            System.err.println(e.getMessage());
+        } catch (IOException e2) {
+            System.err.println(e2.getMessage());
         }
+        return bos != null ? bos.toByteArray() : null;
     }
 }
