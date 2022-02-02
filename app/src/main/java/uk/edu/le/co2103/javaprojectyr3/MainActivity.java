c52754d7994562b063665c897499b74db1740cec
ernisernis <ernestas.dainys@gmail.com>
2022-02-02 13:16:51 +0000

    Generating a key using block mode GCM

--- app/src/main/java/uk/edu/le/co2103/javaprojectyr3/MainActivity.java
+++ app/src/main/java/uk/edu/le/co2103/javaprojectyr3/MainActivity.java
@@ -4,12 +4,31 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.security.keystore.KeyGenParameterSpec;
+import android.security.keystore.KeyProperties;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    Button clickMe, button2;
+    Button clickMe, button2, button3,button4;
 
 
     @Override
@@ -17,9 +36,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         clickMe = findViewById(R.id.button);
         button2 = findViewById(R.id.button2);
-
+        button3 = findViewById(R.id.button3);
+        button4 = findViewById(R.id.button4);
 
         clickMe.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,5 +59,79 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                KeyGenerator keyGenerator = null;
+                try {
+                    keyGenerator = KeyGenerator.getInstance(
+                            KeyProperties.KEY_ALGORITHM_AES, "AndroidKeyStore");
+                } catch (NoSuchAlgorithmException e) {
+                    e.printStackTrace();
+                } catch (NoSuchProviderException e) {
+                    e.printStackTrace();
+                }
+                try {
+                    keyGenerator.init(
+                            new KeyGenParameterSpec.Builder("key2",
+                                    KeyProperties.PURPOSE_ENCRYPT | KeyProperties.PURPOSE_DECRYPT)
+                                    .setBlockModes(KeyProperties.BLOCK_MODE_GCM)
+                                    .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_NONE)
+                                    .build());
+                    Toast.makeText(MainActivity.this, "I am here!", Toast.LENGTH_SHORT).show();
+                } catch (InvalidAlgorithmParameterException e) {
+                    e.printStackTrace();
+                }
+                SecretKey key = keyGenerator.generateKey();
+
+                Cipher cipher = null;
+                try {
+                    cipher = Cipher.getInstance("AES/GCM/NoPadding");
+                } catch (NoSuchAlgorithmException e) {
+                    e.printStackTrace();
+                } catch (NoSuchPaddingException e) {
+                    e.printStackTrace();
+                }
+                try {
+                    cipher.init(Cipher.ENCRYPT_MODE, key);
+                } catch (InvalidKeyException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                KeyStore keyStore = null;
+                try {
+                    keyStore = KeyStore.getInstance("AndroidKeyStore");
+                } catch (KeyStoreException e) {
+                    e.printStackTrace();
+                }
+                try {
+                    keyStore.load(null);
+                } catch (CertificateException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (NoSuchAlgorithmException e) {
+                    e.printStackTrace();
+                }
+                try {
+                    SecretKey key = (SecretKey) keyStore.getKey("key2", null);
+//                    Toast.makeText(MainActivity.this, (CharSequence) key, Toast.LENGTH_SHORT).show();
+                    System.out.println(key);
+                } catch (KeyStoreException e) {
+                    e.printStackTrace();
+                } catch (NoSuchAlgorithmException e) {
+                    e.printStackTrace();
+                } catch (UnrecoverableKeyException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
     }
 }
