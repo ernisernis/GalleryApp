e5d2c5424dbfeb468fb74d2a6abee0a7b4820933
ernisernis <ernestas.dainys@gmail.com>
2022-02-03 14:54:01 +0000

    Created 16length random password

--- app/src/main/java/uk/edu/le/co2103/javaprojectyr3/MainActivity.java
+++ app/src/main/java/uk/edu/le/co2103/javaprojectyr3/MainActivity.java
@@ -2,7 +2,9 @@ package uk.edu.le.co2103.javaprojectyr3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.security.keystore.KeyGenParameterSpec;
 import android.security.keystore.KeyProperties;
@@ -19,6 +21,9 @@ import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
 import java.security.UnrecoverableKeyException;
 import java.security.cert.CertificateException;
+import java.util.Arrays;
+import java.util.Enumeration;
+import java.util.Random;
 
 import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
@@ -28,7 +33,7 @@ import javax.crypto.SecretKey;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button clickMe, button2, button3,button4;
+    Button clickMe, button2, button3,button4, button5, button6;
 
 
     @Override
@@ -41,6 +46,8 @@ public class MainActivity extends AppCompatActivity {
         button2 = findViewById(R.id.button2);
         button3 = findViewById(R.id.button3);
         button4 = findViewById(R.id.button4);
+        button5 = findViewById(R.id.button5);
+        button6 = findViewById(R.id.button6);
 
         clickMe.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -74,28 +81,15 @@ public class MainActivity extends AppCompatActivity {
                 }
                 try {
                     keyGenerator.init(
-                            new KeyGenParameterSpec.Builder("key2",
-                                    KeyProperties.PURPOSE_ENCRYPT | KeyProperties.PURPOSE_DECRYPT)
+                            new KeyGenParameterSpec.Builder("key6",
+                                        KeyProperties.PURPOSE_ENCRYPT | KeyProperties.PURPOSE_DECRYPT)
+//                                    KeyProperties.PURPOSE_ENCRYPT | KeyProperties.PURPOSE_DECRYPT)
                                     .setBlockModes(KeyProperties.BLOCK_MODE_GCM)
                                     .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_NONE)
                                     .build());
+                    keyGenerator.generateKey();
                     Toast.makeText(MainActivity.this, "I am here!", Toast.LENGTH_SHORT).show();
                 } catch (InvalidAlgorithmParameterException e) {
-                    e.printStackTrace();
-                }
-                SecretKey key = keyGenerator.generateKey();
-
-                Cipher cipher = null;
-                try {
-                    cipher = Cipher.getInstance("AES/GCM/NoPadding");
-                } catch (NoSuchAlgorithmException e) {
-                    e.printStackTrace();
-                } catch (NoSuchPaddingException e) {
-                    e.printStackTrace();
-                }
-                try {
-                    cipher.init(Cipher.ENCRYPT_MODE, key);
-                } catch (InvalidKeyException e) {
                     e.printStackTrace();
                 }
             }
@@ -120,18 +114,77 @@ public class MainActivity extends AppCompatActivity {
                     e.printStackTrace();
                 }
                 try {
-                    SecretKey key = (SecretKey) keyStore.getKey("key2", null);
+                    KeyStore keyStoreA = KeyStore.getInstance("AndroidKeyStore");
+                    keyStoreA.load(null);
+                    System.out.println("-------------");
+                    System.out.println(keyStoreA.containsAlias("key6"));
+                    System.out.println("-------------");
+                    SecretKey keyA = (SecretKey) keyStoreA.getKey("key6",null);
+                    System.out.println(keyA);
+
+                    System.out.println("0000000000000000000000");
+
+                    KeyStore keyStoreB = KeyStore.getInstance("AndroidKeyStore");
+                    keyStoreB.load(null);
+                    System.out.println("-------------");
+                    System.out.println(keyStoreB.containsAlias("key6"));
+                    System.out.println("-------------");
+//                    System.out.println(keyStoreB.getKey("key5",null));
+                    SecretKey keyB = (SecretKey) keyStoreB.getKey("key6",null);
+                    System.out.println(keyB);
+                    ////////////
+//                    SecretKey key = (SecretKey) keyStore.getKey("key2", null);
 //                    Toast.makeText(MainActivity.this, (CharSequence) key, Toast.LENGTH_SHORT).show();
-                    System.out.println(key);
+//                    System.out.println(key);
                 } catch (KeyStoreException e) {
                     e.printStackTrace();
                 } catch (NoSuchAlgorithmException e) {
+                    e.printStackTrace();
+                } catch (CertificateException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
                     e.printStackTrace();
                 } catch (UnrecoverableKeyException e) {
                     e.printStackTrace();
                 }
             }
         });
+        button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+                SharedPreferences sharedpreferences = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedpreferences.edit();
+                editor.putString("nameKey", "Hello");
+                editor.commit();
+                Toast.makeText(MainActivity.this, "Button5 Pressed!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        button6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                char[] abc = generatePassword();
+//                System.out.println(Arrays.toString(abc));
+                String stringPW = new String(abc);
+                System.out.println(stringPW);
+                Toast.makeText(MainActivity.this, "Button6 clicked!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+
+    private static char[] generatePassword(){
+        String letters = "ABCDEFGHIJKLMNOPRQSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+        String numbers = "0123456789";
+        String specialChar = "!@#$%^&*";
+
+        String pwa = letters+numbers+specialChar;
+        Random r = new Random();
+        char[] newPass = new char[32];
+        for (int i = 0; i<32;i++) {
+            newPass[i] = pwa.charAt(r.nextInt(pwa.length()));
+        }
+        return newPass;
     }
 }
