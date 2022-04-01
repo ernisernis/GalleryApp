4ac91e347acfae7b6c284a504ca5b6914dd7ec47
ernisernis <ernestas.dainys@gmail.com>
2022-04-01 15:37:59 +0100

    Added missing files

---
+++ app/src/main/java/uk/edu/le/co2103/javaprojectyr3/ImageActivity.java
@@ -0,0 +1,14 @@
+package uk.edu.le.co2103.javaprojectyr3;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ImageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image);
+    }
+}
